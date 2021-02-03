08a2fbcb5bd2ab3da9477aa125658a808722af42
Luca Berger <kucki10@hotmail.com>
2021-02-03 15:48:30 +0100

    Renamed res folder to resources

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     auto result = templatelib::type(10);
     std::cout << "Type: " << result._value << std::endl;
 
-    String image_path = R"(res\color_image.jpg)";
+    String image_path = R"(resources\color_image.jpg)";
     Mat img = imread(image_path, IMREAD_COLOR);
     if(img.empty())
     {
