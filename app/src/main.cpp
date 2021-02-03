ad54396ef8406e901a15591052bff1b4f103bc41
Luca Berger <kucki10@hotmail.com>
2021-02-03 12:54:39 +0100

    Added resource treatment for simple demo app (showing an image with opencv)

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,8 +1,24 @@
 #include <template_lib/template_lib.hpp>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
 
 int main(int argc, char* argv[]) {
     auto result = templatelib::type(10);
     std::cout << "Type: " << result._value << std::endl;
+
+    String image_path = R"(res\color_image.jpg)";
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if(img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+
     return 0;
 }
